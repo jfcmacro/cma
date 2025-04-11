94c1af674cb1cf21858626e7c25111fe4e4ac940
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2025-04-11 06:49:14 -0500

    Correcting test_other_worst_fit

--- tests/test_other_worst_fit.py
+++ tests/test_other_worst_fit.py
@@ -9,9 +9,9 @@ class TestBasicFirstFit(unittest.TestCase):
     MEMMAP = read_memmap_file("resources/memmap/memmap_2.txt")
     REQS = read_reqs_file("resources/reqs/reqs_2.txt")
     
-    @parameterized.expand([(1,0x000000,0x014000),
-                           (2,0x04b000,0x01E000),
-                           (4,0x07D000,0x046000),
+    @parameterized.expand([(1,0x07d000,0x014000),
+                           (2,0x091000,0x01E000),
+                           (3,0x0af000,0x046000),
                            ])
     def test_request(self,max_req,expected_base,expected_limit):
         work_memory = self.MEMMAP.copy()
@@ -26,11 +26,11 @@ class TestBasicFirstFit(unittest.TestCase):
         self.assertEqual(base, expected_base)
         self.assertEqual(limit, expected_limit)
 
-    @parameterized.expand([(1,(0x014000,0x037000)),
-                           (2,(0x069000,0x014000)),
-                           # (3,(0x0e1000,0x019000)),
-                           (4,(0x0c3000,0x037000)),
-                           (5,(0x043800,0x007800)),
+    @parameterized.expand([(1,(0x091000,0x069000)),
+                           (2,(0x0af000,0x04b000)),
+                           (3,(0x0f5000,0x005000)),
+                           (4,(0x03e800,0x0c800)),
+                          # (5,(0x043800,0x007800)),
                            ])
     def test_wm_request(self,max_req,expected_memmap):
         work_memory = self.MEMMAP.copy()
